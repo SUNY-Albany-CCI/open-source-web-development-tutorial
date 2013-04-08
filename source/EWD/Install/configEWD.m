b0ac3513cd2941edfb8427986873c8283ee0d29e
Luis Ibanez <luis.ibanez@kitware.com>
2013-04-08 07:39:35 -0400

    Put the resources dir under www.
    To make it more uniform when seen from the web server.

--- source/EWD/Install/configEWD.m
+++ source/EWD/Install/configEWD.m
@@ -6,10 +6,10 @@ setup ; Set the configuration of EWD environment.
  set ^zewd("config","defaultTechnology")="gtm"
  set ^zewd("config","frontEndTechnology")="gtm"
  set ^zewd("config","jsScriptPath","gtm","mode")="fixed"
- set ^zewd("config","jsScriptPath","gtm","outputPath")="/INF362-EWD/resources"
+ set ^zewd("config","jsScriptPath","gtm","outputPath")="/INF362-EWD/www/resources"
  set ^zewd("config","jsScriptPath","gtm","path")="/resources/"
  set ^zewd("config","routinePath","gtm")="/INF362-EWD/gtm/r/"
  set ^zewd("config","sessionDatabase")="gtm"
- ; 
+ ;
  zwrite ^zewd("config",*)
 
